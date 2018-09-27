274d98fc0d3e8e52ace0e375d67734e79719929f
gjhex <jefferis@mrc-lmb.cam.ac.uk>
2018-09-27 18:01:00 +0100

    put flybrain first in LD_LIBRARY_PATH

--- flybrain.sh
+++ flybrain.sh
@@ -1,3 +1,4 @@
 export PATH="/public/flybrain/R/:/public/flybrain/cmtk/lib/cmtk/bin/:$PATH"
-setenv LD_LIBRARY_PATH ${LD_LIBRARY_PATH}:/public/flybrain/R/lib
+export LD_LIBRARY_PATH="/public/flybrain/R/lib:${LD_LIBRARY_PATH}"
+
 
